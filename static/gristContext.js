d0bb5bef69bcbe6ad7abe8f666438a72678c085a
Tarik Amar <1045351+at-github@users.noreply.github.com>
2025-10-15 11:27:00 +0200

    feat(Configuration): Prévenir l'utilisateur d'autoriser le widget

--- static/gristContext.js
+++ static/gristContext.js
@@ -3,21 +3,26 @@ async function getGristContext() {
   let gristUserId = '';
   let gristDocId = '';
 
-  if (typeof grist !== 'undefined') {
-    try {
-      grist.ready();
-      const tokenInfo = await grist.docApi.getAccessToken({readOnly: false});
-      const payload = JSON.parse(atob(tokenInfo.token.split('.')[1]));
-      const {docId, userId} = payload;
+  if (typeof grist === 'undefined')
+    throw new Error('grist not available')
 
-      if (userId && docId) {
-        gristParams = `?grist_user_id=${encodeURIComponent(userId)}&grist_doc_id=${encodeURIComponent(docId)}`;
-        gristUserId = userId;
-        gristDocId = docId;
-      }
-    } catch (error) {
-      console.warn('Contexte Grist non disponible ou erreur:', error);
+  try {
+    grist.ready();
+
+    const tokenInfo = await grist.docApi.getAccessToken({readOnly: false});
+    const payload = JSON.parse(atob(tokenInfo.token.split('.')[1]));
+    const {docId, userId} = payload;
+
+    if (userId && docId) {
+      gristParams = `?grist_user_id=${encodeURIComponent(userId)}&grist_doc_id=${encodeURIComponent(docId)}`;
+      gristUserId = userId;
+      gristDocId = docId;
+    } else {
+      throw new Error('Impossible de récupérer le user id ou le doc id')
     }
+  } catch (error) {
+    console.warn('Contexte Grist non disponible ou erreur:', error);
+    throw new Error('Veuillez donner au widget l’accès complet au document')
   }
 
   return { params: gristParams, userId: gristUserId, docId: gristDocId };
