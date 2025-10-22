4c0515bf1bb86b39c9a7f204cda188bf52d1e606
Tarik Amar <1045351+at-github@users.noreply.github.com>
2025-10-22 13:52:06 +0200

    ci(Tests): Suite de tests terminée pour gristContext.js

--- static/gristContext.js
+++ static/gristContext.js
@@ -1,31 +1,31 @@
 async function getGristContext() {
-  let gristParams = '';
-  let gristUserId = '';
-  let gristDocId = '';
+  let gristParams = ''
+  let gristUserId = ''
+  let gristDocId = ''
 
   if (typeof grist === 'undefined')
     throw new Error('grist not available')
 
   try {
-    grist.ready();
+    grist.ready()
 
-    const tokenInfo = await grist.docApi.getAccessToken({readOnly: false});
-    const payload = JSON.parse(atob(tokenInfo.token.split('.')[1]));
-    const {docId, userId} = payload;
+    const tokenInfo = await grist.docApi.getAccessToken({readOnly: false})
+    const payload = JSON.parse(atob(tokenInfo.token.split('.')[1]))
+    const {docId, userId} = payload
 
     if (userId && docId) {
-      gristParams = `?grist_user_id=${encodeURIComponent(userId)}&grist_doc_id=${encodeURIComponent(docId)}`;
-      gristUserId = userId;
-      gristDocId = docId;
+      gristParams = `?grist_user_id=${encodeURIComponent(userId)}&grist_doc_id=${encodeURIComponent(docId)}`
+      gristUserId = userId
+      gristDocId = docId
     } else {
       throw new Error('Impossible de récupérer le user id ou le doc id')
     }
   } catch (error) {
-    console.warn('Contexte Grist non disponible ou erreur:', error);
+    console.warn('Contexte Grist non disponible ou erreur :', error)
     throw new Error('Veuillez donner au widget l’accès complet au document')
   }
 
-  return { params: gristParams, userId: gristUserId, docId: gristDocId };
+  return { params: gristParams, userId: gristUserId, docId: gristDocId }
 }
 
 module.exports = { getGristContext }
