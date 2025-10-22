6279a7d56ffeb4a7600e235f58df61913ddb4242
Tarik Amar <1045351+at-github@users.noreply.github.com>
2025-10-22 13:35:07 +0200

    ci(Tests): Second test pour gristContext.js

--- tests/gristContext.test.js
+++ tests/gristContext.test.js
@@ -1,11 +1,18 @@
 const { getGristContext }  = require('../static/gristContext.js')
 
-describe('getGristContext', () => {
+it(
+  'Should throw error if grist global is undefined',
+  async () => {
+    await expect(getGristContext()).rejects.toThrow('grist not available');
+  }
+)
 
-  it(
-    'Should throw error if grist global is undefined',
-    async () => {
-      await expect(getGristContext()).rejects.toThrow('grist not available');
-    }
-  )
-})
+it(
+  'Should throw error if docId or userId is missing',
+  async () => {
+    global.grist = { ready: jest.fn(), docApi: { getAccessToken: jest.fn() } };
+    grist.docApi.getAccessToken.mockResolvedValue({ token: 'header.eyJ1c2VySWQiOiIxMjMifQ.signature' });
+
+    await expect(getGristContext()).rejects.toThrow('Veuillez donner au widget l’accès complet au document');
+  }
+)
