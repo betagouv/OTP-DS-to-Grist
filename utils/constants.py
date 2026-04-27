97fcab7902652c03530c102de3387dc872cac081
Tarik Amar <1045351+at-github@users.noreply.github.com>
2026-04-27 15:22:05 +0200

    feat(Synchro): Ajout de types

--- utils/constants.py
+++ utils/constants.py
@@ -1,9 +1,9 @@
 import os
 
 # Constantes partagées pour toute l'application
-DEMARCHES_API_URL = "https://www.demarches-simplifiees.fr/api/v2/graphql"
+DEMARCHES_API_URL: str = "https://www.demarches-simplifiees.fr/api/v2/graphql"
 
-CHANGELOG_PATH = os.path.join(os.path.dirname(__file__), "CHANGELOG.md")
-GITHUB_CHANGELOG_BASE_URL = "https://github.com/betagouv/OTP-DS-to-Grist/blob/main/CHANGELOG.md"
+CHANGELOG_PATH: str = os.path.join(os.path.dirname(__file__), "CHANGELOG.md")
+GITHUB_CHANGELOG_BASE_URL: str = "https://github.com/betagouv/OTP-DS-to-Grist/blob/main/CHANGELOG.md"
 
-EXIT_CODE_EXTERNAL_API_ERROR = 2
+EXIT_CODE_EXTERNAL_API_ERROR: int = 2
