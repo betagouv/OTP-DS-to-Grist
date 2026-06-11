30a8c1e7cbbada007a927096d86153b34888e1a5
Tarik Amar <1045351+at-github@users.noreply.github.com>
2026-06-11 15:36:30 +0200

    chore(DSFR): Use dsfr input to get grist context

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
+const grist = window.grist
 const context = ref(null)
 const error = ref(null)
+
+const userId = ref('')
+const docId = ref('')
+const baseUrl = ref('')
 
 onMounted(async () => {
   try {
     context.value = await getGristContext()
+
+    userId.value = context.value.userId
+    docId.value = context.value.docId
+    baseUrl.value = context.value.baseUrl
   } catch (e) {
     error.value = e
   }
@@ -15,10 +24,34 @@ onMounted(async () => {
 </script>
 
 <template>
+  <DsfrInput
+    v-model="userId"
+    label="Grist user id"
+    placeholder="xxx"
+    hint="Se rempli automatiquement"
+    required
+  />
+  <DsfrInput
+    v-model="docId"
+    label="Grist doc id"
+    placeholder="xxx"
+    hint="Se rempli automatiquement"
+    required
+  />
+  <DsfrInput
+    v-model="baseUrl"
+    label="Grist baseUrl"
+    placeholder="xxx"
+    hint="Se rempli automatiquement"
+    required
+  />
   <div>
-    <p v-if="grist">Module grist chargé</p>
-    <p v-if="context">Context : {{context}}</p>
-    <p v-if="error">/!\ {{error}}</p>
+    <details v-if="grist">
+      <summary>Module grist chargé</summary>
+      {{JSON.stringify(grist)}}
+    </details>
+    <pre v-if="context">{{context}}</pre>
+    <pre v-if="error">/!\ {{error}}</pre>
   </div>
 </template>
 
