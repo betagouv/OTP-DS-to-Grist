da35a986dd83550fb0315750263eac2cff58b3ed
Tarik Amar <tarik.amar@multi.coop>
2026-06-19 12:04:24 +0200

    feat(multi): Nettoyage du composant grist

--- front/src/components/GristFormSection.vue
+++ front/src/components/GristFormSection.vue
@@ -7,7 +7,6 @@ import {
   DsfrInputGroup
 } from '@gouvminint/vue-dsfr'
 
-const grist = window.grist
 const context = ref(null)
 
 const userId = ref('')
@@ -74,42 +73,8 @@ onMounted(async () => {
         @change="handleGristInputChange"
         label="Grist token"
         placeholder="xxx"
-        hint="Se rempli automatiquement"
         required
       />
     </DsfrAccordion>
   </DsfrAccordionsGroup>
-
-
-  <DsfrInput
-    data-test-id="grist-user-id"
-    v-model="userId"
-    label="Grist user id"
-    placeholder="xxx"
-    hint="Se rempli automatiquement"
-    required
-  />
-  <DsfrInput
-    data-test-id="grist-doc-id"
-    v-model="docId"
-    label="Grist doc id"
-    placeholder="xxx"
-    hint="Se rempli automatiquement"
-    required
-  />
-  <DsfrInput
-    data-test-id="grist-base-url"
-    v-model="baseUrl"
-    label="Grist baseUrl"
-    placeholder="xxx"
-    hint="Se rempli automatiquement"
-    required
-  />
-  <div>
-    <details v-if="grist">
-      <summary>Module grist chargé</summary>
-      {{JSON.stringify(grist)}}
-    </details>
-    <pre v-if="context">{{context}}</pre>
-  </div>
 </template>
