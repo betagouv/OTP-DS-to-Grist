366e37081fd0243cbc906603714990525de81d9d
Tarik <tarik.amar@multi.coop>
2026-09-03 10:30:17 +0200

    feat(Aide): Maj des liens + récupération en variable d'environnement (#487)

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -5,3 +5,6 @@ import os
 # L'URL est intentionnellement invalide pour indiquer qu'aucune vraie DB n'est utilisée
 os.environ['DATABASE_URL'] = 'postgresql://invalid-url-used-only-for-mocks-tests'
 os.environ['ENCRYPTION_KEY'] = 'test-encryption-key-for-tests-32bytes'
+os.environ['HELP_LINK_FAQ']='https://docs.numerique.gouv.fr/docs/f6181e26-8739-4671-85b4-eeaf3e828788/'
+os.environ['HELP_LINK_DN_TOKEN_API']='https://docs.numerique.gouv.fr/docs/cd079f0c-c72b-4704-a59b-7aa5b2e77ff0/'
+os.environ['HELP_LINK_GRIST_API_KEY']='https://docs.numerique.gouv.fr/docs/fb36b922-36c8-4c58-bc64-f08151fd5efe/'
--- utils/constants.py
+++ utils/constants.py
@@ -17,3 +17,15 @@ CHANGELOG_PATH: str = os.path.join(os.path.dirname(__file__), "CHANGELOG.md")
 GITHUB_CHANGELOG_BASE_URL: str = "https://github.com/betagouv/OTP-DS-to-Grist/blob/main/CHANGELOG.md"
 
 EXIT_CODE_EXTERNAL_API_ERROR: int = 2
+
+HELP_LINK_FAQ: str = os.getenv("HELP_LINK_FAQ", "")
+if not HELP_LINK_FAQ:
+    raise ValueError("HELP_LINK_FAQ environment variable is required")
+
+HELP_LINK_DN_TOKEN_API: str = os.getenv("HELP_LINK_DN_TOKEN_API", "")
+if not HELP_LINK_DN_TOKEN_API:
+    raise ValueError("HELP_LINK_DN_TOKEN_API environment variable is required")
+
+HELP_LINK_GRIST_API_KEY: str = os.getenv("HELP_LINK_GRIST_API_KEY", "")
+if not HELP_LINK_GRIST_API_KEY:
+    raise ValueError("HELP_LINK_GRIST_API_KEY environment variable is required")
